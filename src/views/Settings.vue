d7b0d25724df21870c33cf4c676190679e38eb47
yujou0 <abc83327@gmail.com>
2020-11-28 13:18:49 +0800

    加入鬧鐘鈴聲

---
+++ src/views/Settings.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+ #settings
+   b-container
+     b-row
+       b-col(cols="12")
+         b-table(:items="items")
+</template>
+
+<script>
+export default {
+  name: 'Settings',
+  data () {
+    return {
+      items: [
+        {
+          name: '鬧鈴',
+          file: 'alarm.mp3'
+        },
+        {
+          name: 'yay',
+          file: 'yay.mp3'
+        }
+      ]
+    }
+  }
+}
+</script>
