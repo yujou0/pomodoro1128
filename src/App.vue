04c48e725a563dae4db05116e9d1cc73431aaffa
yujou0 <abc83327@gmail.com>
2020-11-28 13:07:45 +0800

    navbar內容設定

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
  #app
   b-navbar(toggleable='lg' type='dark' variant='info')
     b-container
-      b-navbar-brand(href='#') 番茄鐘
+      b-navbar-brand(href='/') 番茄鐘
       b-navbar-toggle(target='nav-collapse')
       b-collapse#nav-collapse(is-nav)
         b-navbar-nav
-          b-nav-item(href='#') 待辦
-          b-nav-item(href='#') 設定
+          b-nav-item(to='/list' active-class="active") 待辦
+          b-nav-item(to='/settings' active-class="active") 設定
+  router-view
 </template>
