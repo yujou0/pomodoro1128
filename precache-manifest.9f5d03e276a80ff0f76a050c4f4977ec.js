7cf68326cbbbceb17d7422a25a77c26e99e6f996
yujou0 <63997329+yujou0@users.noreply.github.com>
2020-12-07 00:16:47 +0000

    Deploying to gh-pages from master 23d23ef2eea071e94c51af20db12b8cc7ccd27e1

---
+++ precache-manifest.9f5d03e276a80ff0f76a050c4f4977ec.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2810235fb290aea4ec0",
+    "url": "css/app.0246febc.css"
+  },
+  {
+    "revision": "3af28afd4f9561ced1ef",
+    "url": "css/chunk-vendors.713ba136.css"
+  },
+  {
+    "revision": "22ae6be15b231d64d750e4402b08423f",
+    "url": "images/bg1.jpg"
+  },
+  {
+    "revision": "8b8c54c0308ce0fb529915734d192092",
+    "url": "images/bg2.jpg"
+  },
+  {
+    "revision": "b0381eb6b724fdec294d09bc47522054",
+    "url": "images/logo.png"
+  },
+  {
+    "revision": "7eeae7f70fafa72bcda29a57c992134b",
+    "url": "index.html"
+  },
+  {
+    "revision": "f2810235fb290aea4ec0",
+    "url": "js/app.76516de1.js"
+  },
+  {
+    "revision": "3af28afd4f9561ced1ef",
+    "url": "js/chunk-vendors.c262e5b0.js"
+  },
+  {
+    "revision": "50aa86a921beaba5caac",
+    "url": "js/list.cd0cf61b.js"
+  },
+  {
+    "revision": "1c1720aa8e5dd869222d",
+    "url": "js/settings.9acc7258.js"
+  },
+  {
+    "revision": "980aa8e13662a4e9a80d5106f3ea8aeb",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "965b614dd8fb0cb17e9eac0f0ace01fb",
+    "url": "sounds/alarm1.mp3"
+  },
+  {
+    "revision": "f1f06ef1268c020fec509c687cceb846",
+    "url": "sounds/digitalring.mp3"
+  },
+  {
+    "revision": "bf749a4a6e66551a0bd30ec5d93691ef",
+    "url": "sounds/ringbug.mp3"
+  },
+  {
+    "revision": "46c744f5e53a2259c87f4b029cb43e03",
+    "url": "sounds/yay.mp3"
+  }
+]);
